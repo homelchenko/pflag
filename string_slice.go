5cf6a3acb81d1a9c7ef73bbde953df8ed957caa6
Hennadii Omelchenko <hennadii@unity3d.com>
2020-04-01 23:41:35 +0300

    Restructure file layout to follow newspaper style

--- string_slice.go
+++ string_slice.go
@@ -12,33 +12,6 @@ type stringSliceValue struct {
 	changed bool
 }
 
-func newStringSliceValue(val []string, p *[]string) *stringSliceValue {
-	ssv := new(stringSliceValue)
-	ssv.value = p
-	*ssv.value = val
-	return ssv
-}
-
-func readAsCSV(val string) ([]string, error) {
-	if val == "" {
-		return []string{}, nil
-	}
-	stringReader := strings.NewReader(val)
-	csvReader := csv.NewReader(stringReader)
-	return csvReader.Read()
-}
-
-func writeAsCSV(vals []string) (string, error) {
-	b := &bytes.Buffer{}
-	w := csv.NewWriter(b)
-	err := w.Write(vals)
-	if err != nil {
-		return "", err
-	}
-	w.Flush()
-	return strings.TrimSuffix(b.String(), "\n"), nil
-}
-
 func (s *stringSliceValue) Set(val string) error {
 	v, err := readAsCSV(val)
 	if err != nil {
@@ -53,13 +26,24 @@ func (s *stringSliceValue) Set(val string) error {
 	return nil
 }
 
-func (s *stringSliceValue) Type() string {
-	return "stringSlice"
-}
-
 func (s *stringSliceValue) String() string {
 	str, _ := writeAsCSV(*s.value)
 	return "[" + str + "]"
+}
+
+func writeAsCSV(vals []string) (string, error) {
+	b := &bytes.Buffer{}
+	w := csv.NewWriter(b)
+	err := w.Write(vals)
+	if err != nil {
+		return "", err
+	}
+	w.Flush()
+	return strings.TrimSuffix(b.String(), "\n"), nil
+}
+
+func (s *stringSliceValue) Type() string {
+	return "stringSlice"
 }
 
 func (s *stringSliceValue) Append(val string) error {
@@ -76,6 +60,15 @@ func (s *stringSliceValue) GetSlice() []string {
 	return *s.value
 }
 
+// GetStringSlice return the []string value of a flag with the given name
+func (f *FlagSet) GetStringSlice(name string) ([]string, error) {
+	val, err := f.getFlagType(name, "stringSlice", stringSliceConv)
+	if err != nil {
+		return []string{}, err
+	}
+	return val.([]string), nil
+}
+
 func stringSliceConv(sval string) (interface{}, error) {
 	sval = sval[1 : len(sval)-1]
 	// An empty string would cause a slice with one (empty) string
@@ -85,13 +78,13 @@ func stringSliceConv(sval string) (interface{}, error) {
 	return readAsCSV(sval)
 }
 
-// GetStringSlice return the []string value of a flag with the given name
-func (f *FlagSet) GetStringSlice(name string) ([]string, error) {
-	val, err := f.getFlagType(name, "stringSlice", stringSliceConv)
-	if err != nil {
-		return []string{}, err
+func readAsCSV(val string) ([]string, error) {
+	if val == "" {
+		return []string{}, nil
 	}
-	return val.([]string), nil
+	stringReader := strings.NewReader(val)
+	csvReader := csv.NewReader(stringReader)
+	return csvReader.Read()
 }
 
 // StringSliceVar defines a string flag with specified name, default value, and usage string.
@@ -124,6 +117,13 @@ func StringSliceVar(p *[]string, name string, defaultValue []string, usage strin
 // StringSliceVarP is like StringSliceVar, but accepts a shorthand letter that can be used after a single dash.
 func StringSliceVarP(p *[]string, name, shorthand string, defaultValue []string, usage string) {
 	CommandLine.VarP(newStringSliceValue(defaultValue, p), name, shorthand, usage)
+}
+
+func newStringSliceValue(val []string, p *[]string) *stringSliceValue {
+	ssv := new(stringSliceValue)
+	ssv.value = p
+	*ssv.value = val
+	return ssv
 }
 
 // StringSlice defines a string flag with specified name, default value, and usage string.
